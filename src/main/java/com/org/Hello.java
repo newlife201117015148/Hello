12b7ae3d23ddb3cae5f67b215d8b264a5d67b62b
wl <234233@qq.com>
2019-10-15 16:34:07 +0800

    测试merge tool by wl

--- src/main/java/com/org/Hello.java
+++ src/main/java/com/org/Hello.java
@@ -6,6 +6,6 @@ public class Hello {
 		System.out.println("hello world !");
 		
 		System.out.println("込込込込   細細細");
-		System.out.println("込込込込  最最最");
+		System.out.println("込込込込  最最最   煉煉煉");
 	}
 }
