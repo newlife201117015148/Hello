91ad39378faf210de17dbd26313673fbb955bbef
wl <234233@qq.com>
2019-10-15 16:02:23 +0800

    root commit

---
+++ src/main/java/com/org/Hello.java
@@ -0,0 +1,8 @@
+package com.org;
+
+public class Hello {
+
+	public void say(){
+		System.out.println("hello world !");
+	}
+}
---
+++ src/test/java/com/org/TestHello.java
@@ -0,0 +1,11 @@
+package com.org;
+
+import org.junit.jupiter.api.Test;
+
+public class TestHello {
+	@Test
+	public void testHello() {
+		// TODO Auto-generated method stub
+		new Hello().say();
+	}
+}
